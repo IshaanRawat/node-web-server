4e7840b7596e08a043baaa2a01a6461ab0517217
Ishaan Rawat <ishaan@ishaanrawat.com>
2017-05-21 17:10:31 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const handlebars = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+handlebars.registerPartials(__dirname + "/views/partials");
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + "/public"));
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} at ${req.url}`;
+
+    fs.appendFile('server.log', log + '\n');
+    next();
+});
+app.use((req, res, next) => {
+    res.render('basic.hbs', {
+        pageTitle: "Site under maintainance!"
+    });
+});
+
+handlebars.registerHelper("getCurrentYear", () => {
+    return new Date().getFullYear();
+});
+handlebars.registerHelper("screamIt", (text) => {
+    return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to the newly created website with express and node.'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('basic.hbs', {
+        pageTitle: "About Page"
+    });
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: "Unable to fulfil your request.",
+        success: false
+    });
+});
+
+app.listen(3000, () => {
+    console.log("Server is up and running on port 3000.");
+});
